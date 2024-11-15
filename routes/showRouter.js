e09766be5d52d64efa71a6d2eb7b4aea9bd362b4
Antony Perez <antonyperez0@yahoo.com>
2024-11-15 13:42:45 -0800

    fixed the postman issues wiht showRouter

--- routes/showRouter.js
+++ routes/showRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const router = express.Router();
+const { body, validationResult } = require("express-validator");
 const { Show, User } = require("../models");
+
+const router = express.Router();
 
 router.get("/", async (req, res) => {
   const shows = await Show.findAll();
@@ -18,24 +20,34 @@ router.get("/:id/users", async (req, res) => {
   res.json(users);
 });
 
-router.put("/:id", async (req, res) => {
-  const show = await Show.findByPk(req.params.id);
-  show.available = req.body.available;
-  await show.save();
-  res.json(show);
-});
+router.put(
+  "/:id",
+  body("title")
+    .isLength({ max: 25 })
+    .withMessage("Title must be 25 characters or less"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const show = await Show.findByPk(req.params.id);
+    if (!show) {
+      return res.status(404).json({ message: "Show not found" });
+    }
+    show.title = req.body.title;
+    show.available = req.body.available;
+    await show.save();
+    res.json(show);
+  }
+);
 
 router.delete("/:id", async (req, res) => {
   const show = await Show.findByPk(req.params.id);
+  if (!show) {
+    return res.status(404).json({ message: "Show not found" });
+  }
   await show.destroy();
   res.json({ message: "Show deleted" });
-});
-
-router.get("/genre/:genre", async (req, res) => {
-  const shows = await Show.findAll({
-    where: { genre: req.params.genre },
-  });
-  res.json(shows);
 });
 
 module.exports = router;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,24 @@
-// routes/userRouter.js
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 const { User, Show } = require("../models");
 
+router.put(
+  "/:id/shows/:showId",
+  body("username").isEmail(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const user = await User.findByPk(req.params.id);
+    const show = await Show.findByPk(req.params.showId);
+    await user.addShow(show);
+    res.json({ message: "Show added to user" });
+  }
+);
+
+module.exports = router;
 router.get("/", async (req, res) => {
   const users = await User.findAll();
   res.json(users);
